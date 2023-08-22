84a719b1ce2324c51027a4068e433d6325fddb72
Hoang Loc <hoanggdloc@gmail.com>
2023-08-22 23:40:55 +0700

    Setup http server

--- chatty-backend/src/setupServer.ts
+++ chatty-backend/src/setupServer.ts
@@ -1,17 +1,23 @@
+import compression from "compression";
+import cookieSession from "cookie-session";
+import cors from "cors";
 import {
   Application,
-  json,
-  urlencoded,
-  Response,
+  NextFunction,
   Request,
-  NextFunction
+  Response,
+  json,
+  urlencoded
 } from "express";
+import "express-async-errors";
+import helmet from "helmet";
+import hpp from "hpp";
 import http from "http";
 
-export class ChattyServer {
-  private app: Application;
+const SERVER_PORT = 5000;
 
-  constructor(app: Application) {
+export class ChattyServer {
+  constructor(private app: Application) {
     this.app = app;
   }
 
@@ -23,17 +29,51 @@ export class ChattyServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["test1", "test2"],
+        maxAge: 24 * 7 * 3600000,
+        secure: false
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+      })
+    );
+  }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "50mb" }));
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
+  }
 
   private routeMiddleware(app: Application): void {}
 
   private globalErrorHandler(app: Application): void {}
 
-  private startServer(app: Application): void {}
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   private createScoketIO(httpServer: http.Server): void {}
 
-  private startHttpServer(httpServer: http.Server): void {}
+  private startHttpServer(httpServer: http.Server): void {
+    httpServer.listen(SERVER_PORT, () => {
+      console.log(`Server running on port ${SERVER_PORT}`);
+    });
+  }
 }
